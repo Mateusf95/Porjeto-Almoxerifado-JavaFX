f6289362ce87a999217b8dc7fa10e7f8883f16ed
Mateusf95 <mateushenrique123@live.com>
2025-12-08 19:50:18 -0300

    Criação da controller da página de Dashboard

---
+++ src/main/java/br/com/almoxerifado/infra/javafx/controller/DashboardController.java
@@ -0,0 +1,54 @@
+package br.com.almoxerifado.infra.javafx.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class DashboardController {
+
+    @FXML
+    private void handleMenuCategories(MouseEvent event) {
+
+    }
+
+    @FXML
+    private void handleMenuChangePassword(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleMenuDashboard(MouseEvent event) {
+
+    }
+
+    @FXML
+    private void handleMenuExit(MouseEvent event) {
+
+    }
+
+    @FXML
+    private void handleMenuOrder(MouseEvent event) {
+
+    }
+
+    @FXML
+    private void handleMenuProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleMenuReports(MouseEvent event) {
+
+    }
+
+    @FXML
+    private void handleMenuUsers(MouseEvent event) {
+
+    }
+
+    @FXML
+    private void handleMenuMaterial(MouseEvent event) {
+
+    }
+
+}
